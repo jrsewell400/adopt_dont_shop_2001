fb78096f9083c8cc2f6a4c423ee316c0abc82531
Jordan Sewell <jrsewell400@gmail.com>
2020-03-19 08:53:56 -0600

    Add route for Pets Index page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
+
+  #pets
+  get '/pets', to: 'pets#index'
 end
