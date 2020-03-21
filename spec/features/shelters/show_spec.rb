a9ce4368eb68d553c9aa79276daa5951067d3e27
Jordan Sewell <jrsewell400@gmail.com>
2020-03-21 15:59:55 -0600

    Add test to Shelters show spec to ensure  each shelter has a link to it's pets adoption page

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -12,13 +12,34 @@ RSpec.describe "As a visitor", type: :feature do
       visit "/shelters/#{shelter_1.id}"
 
       expect(page).to have_content(shelter_1.name)
-
       within "#shelter-info" do
         expect(page).to have_content(shelter_1.address)
         expect(page).to have_content(shelter_1.city)
         expect(page).to have_content(shelter_1.state)
         expect(page).to have_content(shelter_1.zip)
       end
+    end
+
+    it "then I see a link to take me to that shelter's pets page /shelters/:id/pets." do 
+      shelter_1 = Shelter.create(name: "Jordan's Shelter",
+                                 address: "123 Fake St.",
+                                 city: "Arvada", 
+                                 state: "CO",
+                                 zip: 80003)
+
+      visit "/shelters/#{shelter_1.id}"
+
+      expect(page).to have_content(shelter_1.name)
+      within "#shelter-info" do
+        expect(page).to have_content(shelter_1.address)
+        expect(page).to have_content(shelter_1.city)
+        expect(page).to have_content(shelter_1.state)
+        expect(page).to have_content(shelter_1.zip)
+        expect(page).to have_link("This Shelter's Pets Page")
+        click_on("This Shelter's Pets Page")
+      end
+      save_and_open_page
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/pets")
     end 
   end
 end
