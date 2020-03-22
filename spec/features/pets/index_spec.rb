3f9a954c2242f44aaad24bea1f9811ee75aebe5a
Jordan Sewell <jrsewell400@gmail.com>
2020-03-22 16:09:13 -0600

    Update test status attribute to use boolean instead of string

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -18,21 +18,21 @@ RSpec.describe "As a visitor,", type: :feature do
       luna = Pet.create(name: "Luna",
                         age: "5",
                         sex: "Female",
-                        status: "Adoptable",
+                        status: true,
                         image: "http://cdn.akc.org/content/article-body-image/norwegianelkhoundpuppy_dog_pictures.jpg",
                         shelter: shelter_1)
 
       nova = Pet.create(name: "Nova",
                         age: "10",
                         sex: "Female",
-                        status: "Adoptable",
+                        status: true,
                         image: "http://cdn.akc.org/content/article-body-image/border_collie_dog_pictures_.jpg",
                         shelter: shelter_1)
 
       roomba = Pet.create(name: "Roomba",
                         age: "7",
                         sex: "Male",
-                        status: "Not Adoptable",
+                        status: false,
                         image: "http://cdn.akc.org/content/article-body-image/basset_hound_dog_pictures_.jpg",
                         shelter: shelter_2)
 
@@ -86,14 +86,14 @@ RSpec.describe "As a visitor,", type: :feature do
       luna = Pet.create(name: "Luna",
                         age: "5",
                         sex: "Female",
-                        status: "Adoptable",
+                        status: true,
                         image: "http://cdn.akc.org/content/article-body-image/norwegianelkhoundpuppy_dog_pictures.jpg",
                         shelter: shelter_1)
 
       nova = Pet.create(name: "Nova",
                         age: "10",
                         sex: "Female",
-                        status: "Adoptable",
+                        status: true,
                         image: "http://cdn.akc.org/content/article-body-image/border_collie_dog_pictures_.jpg",
                         shelter: shelter_1)
 
@@ -149,21 +149,21 @@ RSpec.describe "As a visitor,", type: :feature do
       luna = Pet.create(name: "Luna",
                         age: "5",
                         sex: "Female",
-                        status: "Adoptable",
+                        status: true,
                         image: "http://cdn.akc.org/content/article-body-image/norwegianelkhoundpuppy_dog_pictures.jpg",
                         shelter: shelter_1)
 
       nova = Pet.create(name: "Nova",
                         age: "10",
                         sex: "Female",
-                        status: "Adoptable",
+                        status: true,
                         image: "http://cdn.akc.org/content/article-body-image/border_collie_dog_pictures_.jpg",
                         shelter: shelter_1)
 
       roomba = Pet.create(name: "Roomba",
                           age: "7",
                           sex: "Male",
-                          status: "Not Adoptable",
+                          status: false,
                           image: "http://cdn.akc.org/content/article-body-image/basset_hound_dog_pictures_.jpg",
                           shelter: shelter_2)
 
