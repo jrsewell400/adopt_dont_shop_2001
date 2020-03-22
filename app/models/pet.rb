e28274a5a84eb659ad6b42168b0e406e0fc366bb
Jordan Sewell <jrsewell400@gmail.com>
2020-03-22 16:13:51 -0600

    Update Pet model to include validations for status being a boolean, age being greater than zero as well as between 1 and 25, and created "adoptable?" method to handle displaying certain text depending on whether or not the status is true or false

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,11 @@
 class Pet < ApplicationRecord 
-  validates_presence_of :name, :age, :sex, :status, :image
-
+  validates_presence_of :name, :age, :sex, :image
   belongs_to :shelter
+  validates_inclusion_of :status, :in => [true, false]
+  validates_inclusion_of :age, in: 1..25
+  validates_numericality_of :age, greater_than: 0
+
+  def adoptable?
+    status ? "Adoptable" : "Pending Adoption"
+  end
 end 
